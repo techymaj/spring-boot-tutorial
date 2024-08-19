3604a887d7a8f2d92d586c4916f5aedae26b8f9b
Wilfried M Majaliwa <wilfriedmajaliwa@gmail.com>
2024-08-19 16:53:59 +0300

    Beans Intro

---
+++ src/main/java/tech/majaliwa/content_calendar/MessageDest.java
@@ -0,0 +1,11 @@
+package tech.majaliwa.content_calendar;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class MessageDest {
+
+    public void destination() {
+        System.out.println("You have reached");
+    }
+}
