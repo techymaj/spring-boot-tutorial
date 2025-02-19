21378f50339f611f750d9646c0713b561725a6d1
Wilfried M Majaliwa <wilfriedmajaliwa@gmail.com>
2025-02-19 11:27:11 +0300

    Spring Data REST configs

--- src/main/java/tech/majaliwa/EmployeeCRUD/dao/EmployeeRepository.java
+++ src/main/java/tech/majaliwa/EmployeeCRUD/dao/EmployeeRepository.java
@@ -1,7 +1,9 @@
 package tech.majaliwa.EmployeeCRUD.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import tech.majaliwa.EmployeeCRUD.models.Employee;
 
+@RepositoryRestResource(path = "members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 }
